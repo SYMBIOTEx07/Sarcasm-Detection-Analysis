1026c3218d795cabdcd5e79ce38e51b929456deb
SYMBIOTEx07 <83158467+SYMBIOTEx07@users.noreply.github.com>
2023-10-21 19:46:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,465 @@
+import random
 import streamlit as st
+import numpy as np
+import pandas as pd
+import os
+import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import re
+from collections import Counter
+from wordcloud import WordCloud, ImageColorGenerator
+import matplotlib.pyplot as plt
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, GRU, LSTM, Bidirectional
+from tensorflow.keras.layers import Embedding
+from keras.initializers import Constant
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
 
-st.write('Hello world!')
+sarcastic_responses = [
+"Oh, how original. I've never heard that one before.",
+"You must be a mind reader with those insights.",
+"What an exciting conversation. My heart is racing.",
+"I can see your intelligence is blinding.",
+"Your sarcasm is truly awe-inspiring.",
+"Brilliant observation, Captain Obvious.",
+"Your sense of humor is... something.",
+"Keep talking; I'm hanging on your every word.",
+"I'm amazed by your vast knowledge on the subject.",
+"You're right, I should take notes on your wisdom."
+]
+
+responses = [
+"I understand your point of view.",
+"Thank you for sharing that.",
+"That's a valid perspective.",
+"I appreciate your input.",
+"Let's consider this viewpoint.",
+"Your feedback is valuable.",
+"I respect your opinion.",
+"This is a good addition to our discussion.",
+"You've given us something to think about.",
+"Thanks for contributing to the conversation."
+]
+
+data_1 = pd.read_json("./archive/Sarcasm_Headlines_Dataset.json", lines=True)
+data_2 = pd.read_json("./archive/Sarcasm_Headlines_Dataset_v2.json", lines=True)
+data =  pd.concat([data_1, data_2])
+data.head()
+
+def clean_text(text):
+    text = text.lower()
+
+    pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    text = pattern.sub('', text)
+    text = " ".join(filter(lambda x: x[0] != '@', text.split()))
+    emoji = re.compile("["
+                       u"\U0001F600-\U0001FFFF"  # emoticons
+                       u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                       u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                       u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                       u"\U00002702-\U000027B0"
+                       u"\U000024C2-\U0001F251"
+                       "]+", flags=re.UNICODE)
+
+    text = emoji.sub(r'', text)
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"don't", "do not", text)
+    text = re.sub(r"did't", "did not", text)
+    text = re.sub(r"can't", "can not", text)
+    text = re.sub(r"it's", "it is", text)
+    text = re.sub(r"couldn't", "could not", text)
+    text = re.sub(r"have't", "have not", text)
+    text = re.sub(r"[,.\"\'!@#$%^&*(){}?/;`~:<>+=-]", "", text)
+    return text
+
+def CleanTokenize(df):
+    head_lines = list()
+    lines = df["headline"].values.tolist()
+
+    for line in lines:
+        line = clean_text(line)
+        # tokenize the text
+        tokens = word_tokenize(line)
+        # remove puntuations
+        table = str.maketrans('', '', string.punctuation)
+        stripped = [w.translate(table) for w in tokens]
+        # remove non alphabetic characters
+        words = [word for word in stripped if word.isalpha()]
+        stop_words = set(stopwords.words("english"))
+        # remove stop words
+        words = [w for w in words if not w in stop_words]
+        head_lines.append(words)
+    return head_lines
+
+head_lines = CleanTokenize(data)
+validation_split = 0.2
+max_length = 25
+
+
+tokenizer_obj = Tokenizer()
+tokenizer_obj.fit_on_texts(head_lines)
+sequences = tokenizer_obj.texts_to_sequences(head_lines)
+
+word_index = tokenizer_obj.word_index
+print("unique tokens - ",len(word_index))
+vocab_size = len(tokenizer_obj.word_index) + 1
+print('vocab size -', vocab_size)
+
+lines_pad = pad_sequences(sequences, maxlen=max_length, padding='post')
+sentiment =  data['is_sarcastic'].values
+
+indices = np.arange(lines_pad.shape[0])
+np.random.shuffle(indices)
+lines_pad = lines_pad[indices]
+sentiment = sentiment[indices]
+
+num_validation_samples = int(validation_split * lines_pad.shape[0])
+
+X_train_pad = lines_pad[:-num_validation_samples]
+y_train = sentiment[:-num_validation_samples]
+X_test_pad = lines_pad[-num_validation_samples:]
+y_test = sentiment[-num_validation_samples:]
+
+embeddings_index = {}
+embedding_dim = 100
+f = open('./glove.twitter.27B.100d.txt', encoding = "utf-8")
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Found %s word vectors.' % len(embeddings_index))
+
+embedding_matrix = np.zeros((len(word_index) + 1, embedding_dim))
+c = 0
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        c+=1
+        embedding_matrix[i] = embedding_vector
+print(c)
+
+embedding_layer = Embedding(len(word_index) + 1,
+                            embedding_dim,
+                            weights=[embedding_matrix],
+                            input_length=max_length,
+                            trainable=False)
+
+model = Sequential()
+model.add(embedding_layer)
+model.add(LSTM(64, dropout=0.2, recurrent_dropout=0.25))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+
+print('Summary of the built model...')
+print(model.summary())
+
+history = model.fit(X_train_pad, y_train, batch_size=32, epochs=25, validation_data=(X_test_pad, y_test), verbose=2)
+
+# Plot results
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc)+1)
+
+plt.plot(epochs, acc, 'g', label='Training accuracy')
+plt.plot(epochs, val_acc, 'r', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'g', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+def predict_sarcasm(s):
+    x_final = pd.DataFrame({"headline":[s]})
+    test_lines = CleanTokenize(x_final)
+    test_sequences = tokenizer_obj.texts_to_sequences(test_lines)
+    test_review_pad = pad_sequences(test_sequences, maxlen=max_length, padding='post')
+    pred = model.predict(test_review_pad)
+    pred*=100
+    if pred[0][0]>=50: return True
+    else: return False
+
+st.title("Chat Bot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = random.choice(sarcastic_responses) if predict_sarcasm(prompt) else random.choice(sarcastic_responses)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})import random
+import streamlit as st
+import numpy as np
+import pandas as pd
+import os
+import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+import re
+from collections import Counter
+from wordcloud import WordCloud, ImageColorGenerator
+import matplotlib.pyplot as plt
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, GRU, LSTM, Bidirectional
+from tensorflow.keras.layers import Embedding
+from keras.initializers import Constant
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
+
+sarcastic_responses = [
+"Oh, how original. I've never heard that one before.",
+"You must be a mind reader with those insights.",
+"What an exciting conversation. My heart is racing.",
+"I can see your intelligence is blinding.",
+"Your sarcasm is truly awe-inspiring.",
+"Brilliant observation, Captain Obvious.",
+"Your sense of humor is... something.",
+"Keep talking; I'm hanging on your every word.",
+"I'm amazed by your vast knowledge on the subject.",
+"You're right, I should take notes on your wisdom."
+]
+
+responses = [
+"I understand your point of view.",
+"Thank you for sharing that.",
+"That's a valid perspective.",
+"I appreciate your input.",
+"Let's consider this viewpoint.",
+"Your feedback is valuable.",
+"I respect your opinion.",
+"This is a good addition to our discussion.",
+"You've given us something to think about.",
+"Thanks for contributing to the conversation."
+]
+
+data_1 = pd.read_json("./archive/Sarcasm_Headlines_Dataset.json", lines=True)
+data_2 = pd.read_json("./archive/Sarcasm_Headlines_Dataset_v2.json", lines=True)
+data =  pd.concat([data_1, data_2])
+data.head()
+
+def clean_text(text):
+    text = text.lower()
+
+    pattern = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+    text = pattern.sub('', text)
+    text = " ".join(filter(lambda x: x[0] != '@', text.split()))
+    emoji = re.compile("["
+                       u"\U0001F600-\U0001FFFF"  # emoticons
+                       u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                       u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                       u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                       u"\U00002702-\U000027B0"
+                       u"\U000024C2-\U0001F251"
+                       "]+", flags=re.UNICODE)
+
+    text = emoji.sub(r'', text)
+    text = text.lower()
+    text = re.sub(r"i'm", "i am", text)
+    text = re.sub(r"he's", "he is", text)
+    text = re.sub(r"she's", "she is", text)
+    text = re.sub(r"that's", "that is", text)
+    text = re.sub(r"what's", "what is", text)
+    text = re.sub(r"where's", "where is", text)
+    text = re.sub(r"\'ll", " will", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"\'re", " are", text)
+    text = re.sub(r"\'d", " would", text)
+    text = re.sub(r"\'ve", " have", text)
+    text = re.sub(r"won't", "will not", text)
+    text = re.sub(r"don't", "do not", text)
+    text = re.sub(r"did't", "did not", text)
+    text = re.sub(r"can't", "can not", text)
+    text = re.sub(r"it's", "it is", text)
+    text = re.sub(r"couldn't", "could not", text)
+    text = re.sub(r"have't", "have not", text)
+    text = re.sub(r"[,.\"\'!@#$%^&*(){}?/;`~:<>+=-]", "", text)
+    return text
+
+def CleanTokenize(df):
+    head_lines = list()
+    lines = df["headline"].values.tolist()
+
+    for line in lines:
+        line = clean_text(line)
+        # tokenize the text
+        tokens = word_tokenize(line)
+        # remove puntuations
+        table = str.maketrans('', '', string.punctuation)
+        stripped = [w.translate(table) for w in tokens]
+        # remove non alphabetic characters
+        words = [word for word in stripped if word.isalpha()]
+        stop_words = set(stopwords.words("english"))
+        # remove stop words
+        words = [w for w in words if not w in stop_words]
+        head_lines.append(words)
+    return head_lines
+
+head_lines = CleanTokenize(data)
+validation_split = 0.2
+max_length = 25
+
+
+tokenizer_obj = Tokenizer()
+tokenizer_obj.fit_on_texts(head_lines)
+sequences = tokenizer_obj.texts_to_sequences(head_lines)
+
+word_index = tokenizer_obj.word_index
+print("unique tokens - ",len(word_index))
+vocab_size = len(tokenizer_obj.word_index) + 1
+print('vocab size -', vocab_size)
+
+lines_pad = pad_sequences(sequences, maxlen=max_length, padding='post')
+sentiment =  data['is_sarcastic'].values
+
+indices = np.arange(lines_pad.shape[0])
+np.random.shuffle(indices)
+lines_pad = lines_pad[indices]
+sentiment = sentiment[indices]
+
+num_validation_samples = int(validation_split * lines_pad.shape[0])
+
+X_train_pad = lines_pad[:-num_validation_samples]
+y_train = sentiment[:-num_validation_samples]
+X_test_pad = lines_pad[-num_validation_samples:]
+y_test = sentiment[-num_validation_samples:]
+
+embeddings_index = {}
+embedding_dim = 100
+f = open('./glove.twitter.27B.100d.txt', encoding = "utf-8")
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype='float32')
+    embeddings_index[word] = coefs
+f.close()
+
+print('Found %s word vectors.' % len(embeddings_index))
+
+embedding_matrix = np.zeros((len(word_index) + 1, embedding_dim))
+c = 0
+for word, i in word_index.items():
+    embedding_vector = embeddings_index.get(word)
+    if embedding_vector is not None:
+        c+=1
+        embedding_matrix[i] = embedding_vector
+print(c)
+
+embedding_layer = Embedding(len(word_index) + 1,
+                            embedding_dim,
+                            weights=[embedding_matrix],
+                            input_length=max_length,
+                            trainable=False)
+
+model = Sequential()
+model.add(embedding_layer)
+model.add(LSTM(64, dropout=0.2, recurrent_dropout=0.25))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+
+print('Summary of the built model...')
+print(model.summary())
+
+history = model.fit(X_train_pad, y_train, batch_size=32, epochs=25, validation_data=(X_test_pad, y_test), verbose=2)
+
+# Plot results
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc)+1)
+
+plt.plot(epochs, acc, 'g', label='Training accuracy')
+plt.plot(epochs, val_acc, 'r', label='Validation accuracy')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs, loss, 'g', label='Training loss')
+plt.plot(epochs, val_loss, 'r', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
+def predict_sarcasm(s):
+    x_final = pd.DataFrame({"headline":[s]})
+    test_lines = CleanTokenize(x_final)
+    test_sequences = tokenizer_obj.texts_to_sequences(test_lines)
+    test_review_pad = pad_sequences(test_sequences, maxlen=max_length, padding='post')
+    pred = model.predict(test_review_pad)
+    pred*=100
+    if pred[0][0]>=50: return True
+    else: return False
+
+st.title("Chat Bot")
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = random.choice(sarcastic_responses) if predict_sarcasm(prompt) else random.choice(sarcastic_responses)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
